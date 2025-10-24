d870f20e3297c1eb60bbafdedac65e9285deb6f3
kunwe <167782990+kunwe@users.noreply.github.com>
2025-10-24 23:16:27 +0200

    Add UserName property and constructor to Feedback model

--- PROG6212 POE PROTOTYPE/Models/Feedback.cs
+++ PROG6212 POE PROTOTYPE/Models/Feedback.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace PROG6212_POE_PROTOTYPE.Models
 {
@@ -7,7 +7,13 @@ namespace PROG6212_POE_PROTOTYPE.Models
         public int FeedbackId { get; set; }
         public int ClaimId { get; set; } // Foreign Key to Claim
         public int UserId { get; set; }  // Foreign Key to the User (Manager/Coordinator) who wrote the feedback
+        public string UserName { get; set; }
         public string Comment { get; set; }
         public DateTime DateAdded { get; set; }
+
+        public Feedback()
+        {
+            DateAdded = DateTime.Now;
+        }
     }
 }
